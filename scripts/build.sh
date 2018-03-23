25119b7030e8adfdab49fff16feff04e6416ec8d
Rob Myers <rob@robmyers.org>
2018-03-23 11:29:47 -0700

    More build permissions.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,8 @@ RRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 pushd "${RRDIR}"
 git pull
 pushd jekyll
-jekyll build
-sudo mkdir -p /var/www/reversionary-rights
-sudo cp -r _site/* /var/www/reversionary-rights/
-sudo chown -R www-data:www-data /var/www/reversionary-rights/
+bundle exec jekyll build
+sudo -u www-data mkdir -p /var/www/reversionary-rights
+sudo -u www-data cp -r _site/* /var/www/reversionary-rights/
 popd
 popd
