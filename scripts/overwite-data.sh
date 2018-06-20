80494ef973dd6ab47c1a4704046ef36a17c0aebe
Rob Myers <rob@robmyers.org>
2018-06-20 15:52:19 -0700

    Begin generating pages from case and scholarship data.

--- scripts/overwite-data.sh
+++ scripts/overwite-data.sh
@@ -4,14 +4,9 @@
 
 RRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-curl 'https://docs.google.com/spreadsheets/d/1c0VIUgFJYSbtTEZThPyoB-FetMvQDPv7FihrsXi6GwA/export?exportFormat=csv' \
-     > "${RRDIR}/jekyll/_data/bills.csv"
+curl 'https://docs.google.com/spreadsheets/d/XXXXXXXX/export?exportFormat=csv' \
+     > "${RRDIR}/jekyll/_data/cases.csv"
 
-curl 'https://docs.google.com/spreadsheets/d/1wtjxgDWgC-jGLVIT1gUpPw9NUzPZa8ShetQlmJWRhnw/export?exportFormat=csv' \
-     > "${RRDIR}/jekyll/_data/decisions.csv"
+curl 'https://docs.google.com/spreadsheets/d/XXXXXXXX/export?exportFormat=csv' \
+     > "${RRDIR}/jekyll/_data/scholarship.csv"
 
-curl 'https://docs.google.com/spreadsheets/d/17G3bnBt-3Bsqw1Ra45kIW8Z4VrhEp2_HhYD13ByfQD4/export?exportFormat=csv' \
-    > "${RRDIR}/jekyll/_data/legislation.csv"
-
-curl 'https://docs.google.com/spreadsheets/d/1Or6UcG2OyddsEpO6WQ2H2XFnJs-0EvJJtXoe18QMydI/export?exportFormat=csv' \
-     > "${RRDIR}/jekyll/_data/resources.csv"
