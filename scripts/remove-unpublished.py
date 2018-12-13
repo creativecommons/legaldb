db53b11c5e8ca9b6fd269bcccaee523e30dac418
Timid Robot Zehta <timid@creativecommons.org>
2018-12-13 16:39:18 -0600

    Updated for consistency
    - Always use double quotes
    - Use newlines for line termination
    - Strip all columns
    - Made output easier to read

--- scripts/remove-unpublished.py
+++ scripts/remove-unpublished.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
 
+"""Clean-up CSV file and list countries referenced.
+"""
+
+# Standard library
 import csv
+import os.path
 import shutil
 import sys
 import tempfile
+
 
 infilename = sys.argv[1]
 
 countries = set()
 
-with open(infilename, newline='') as infile:
+with open(infilename, newline="") as infile:
     csvreader = csv.DictReader(infile)
-    with tempfile.NamedTemporaryFile(mode='w', newline='') as outfile:
+    with tempfile.NamedTemporaryFile(mode="w", newline="") as outfile:
         tmpnam = outfile.name
-        csvwriter = csv.DictWriter(outfile, fieldnames=csvreader.fieldnames)
+        csvwriter = csv.DictWriter(outfile, fieldnames=csvreader.fieldnames,
+                                   lineterminator="\n")
         csvwriter.writeheader()
         for row in csvreader:
-            if row['Publish'] in ['Y', 'y', 'Publish', 'publish']:
-                # Remove trailing whitespace in country fields, as we use
-                # the country directly as a key.
-                row['Country'] = row['Country'].strip()
+            if row["Publish"].strip() in ["Y", "y", "Publish", "publish"]:
+                # Strip leading/trailing whitespace from all columns. This is
+                # especially important for the country column, as we use the
+                # country value as a key.
+                for key in row.keys():
+                    row[key] = row[key].strip()
+                row["Country"] = row["Country"].strip()
                 csvwriter.writerow(row)
-                countries.add(row['Country'])
+                countries.add("  {}".format(row["Country"]))
         shutil.copy(tmpnam, infilename)
 
-print("Countries mentioned in " + infilename + ":")
+print("Countries referenced in " + os.path.basename(infilename) + ":")
 print("\n".join(sorted(countries)))
