78c365b617880015fbf41022c9fc7e99fbd10aec
Timid Robot Zehta <timid@creativecommons.org>
2018-12-13 16:16:42 -0600

    fixed missing CASES GID. minor refactor.

--- scripts/overwrite-data.sh
+++ scripts/overwrite-data.sh
@@ -3,39 +3,42 @@ set -o errexit
 set -o errtrace
 set -o nounset
 
-# Fetch the latest data from Google Docs and overwrite the versions
+# Fetch the latest data from Google Docs and overwrite the current versions
 
 RRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: overwrite-data.sh <SHEETID>"
-    echo "       Where SHEETID is the Google Sheets identifier for Master Spreadsheet."
-    echo "       This can be found after /d/ in the sheet URL and before /edit ."
-    echo "       e.g. 1436GBvNkSCQ3uRR7ZLPUDEZH2rknvuD7ZJ7vFGhWgoM"
-    exit 1
+    echo 'Usage: overwrite-data.sh SHEETID'
+    echo '  Where SHEETID is the Google Sheets identifier for Master Spreadsheet.'
+    echo '  This can be found after "/d/" in the sheet URL and before "/edit".'
+    echo '  ex. 1Z9IcBgdDYoeZw0Xx573ZMp5JcJDCNhOUHrj0guU9byo'
+    exit 2
 fi
 
-SHEETID="${1}"
+SHEET_ID="${1}"
+EXPORT_URL="https://docs.google.com/spreadsheets/d/${SHEET_ID}/export"
+DATA_DIR=${RRDIR}/jekyll/_data
 
+GID=0
+echo '# Downloading Caselaw - Primary Data: CASES to cases.csv'
+echo "# Sheet ID: ${SHEET_ID}"
+echo "# CASES sheet GID: ${GID}"
+curl "${EXPORT_URL}?gid=${GID}&exportFormat=csv" > ${DATA_DIR}/cases.csv
 echo
-echo "Sheet ID: ${SHEETID}"
+
+GID=1441455659
+echo '# Downloading Caselaw - Primary Data: SCHOLARSHIP to scholarship.csv'
+echo "# Sheet ID: ${SHEET_ID}"
+echo "# SCHOLARSHIP sheet GID: ${GID}"
+curl "${EXPORT_URL}?gid=${GID}&exportFormat=csv" > ${DATA_DIR}/scholarship.csv
 echo
-echo 'Downloading Caselaw - Primary Data: CASES sheet to cases.csv'
-curl "https://docs.google.com/spreadsheets/d/${SHEETID}/export?exportFormat=csv" \
-     > "${RRDIR}/jekyll/_data/cases.csv"
-echo 'Done.'
+
+echo '# Removing Publish: N rows from cases.csv'
+${RRDIR}/scripts/remove-unpublished.py ${DATA_DIR}/cases.csv
 echo
-echo 'Downloading Caselaw - Primary Data: SCHOLARSHIP sheet to scholarship.csv'
-curl "https://docs.google.com/spreadsheets/d/${SHEETID}/export?gid=1441455659&exportFormat=csv" \
-     > "${RRDIR}/jekyll/_data/scholarship.csv"
-echo 'Done.'
+
+echo '# Removing Publish: N rows from cases.csv'
+${RRDIR}/scripts/remove-unpublished.py ${DATA_DIR}/scholarship.csv
 echo
-echo 'Removing Publish: N rows from cases.csv'
-"${RRDIR}/scripts/remove-unpublished.py" "${RRDIR}/jekyll/_data/cases.csv"
-echo 'Done.'
-echo
-echo 'Removing Publish: N rows from cases.csv'
-"${RRDIR}/scripts/remove-unpublished.py" "${RRDIR}/jekyll/_data/scholarship.csv"
-echo Done.
-echo
-echo All processes completed successfully.
+
+echo '# All processes completed successfully.'
