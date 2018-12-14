dc5b38ee7beca94f4469b08694a67efac50801ed
Timid Robot Zehta <timid@creativecommons.org>
2018-12-14 09:34:29 -0600

    updated to use rsync, create a backup, and improved output

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
-
+#
 # Pull the latest changes, build them here, then deploy inside /var/www/
 # This hardcodes a lot of assumptions.
+set -o errexit
+set -o errtrace
+set -o nounset
 
 RRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+DIR_LIVE=/var/www/caselaw
+DIR_OLDBAK=/var/www/_OLDBAK.caselaw
 
-pushd "${RRDIR}"
+pushd "${RRDIR}" > /dev/null
+echo '# git pull'
 git pull
-pushd jekyll
+echo
+
+if [[ -d ${DIR_OLDBAK} ]]
+then
+    echo '# removing previous _OLDBAK directory'
+    sudo rm -rf ${DIR_OLDBAK}
+    echo 'removed.'
+    echo
+fi
+
+pushd jekyll >/dev/null
+echo '# jekyll build'
 /usr/local/bin/bundle exec jekyll build \
-                      --config _config.yml,_config-publish.yml
-sudo -u www-data mkdir -p /var/www/caselaw
-sudo -u www-data cp -r _site/* /var/www/caselaw/
-popd
-popd
+    --config _config.yml,_config-publish.yml
+echo
+
+echo '# copying hosting directory to _OLDBAK directory'
+sudo cp -a ${DIR_LIVE} ${DIR_OLDBAK}
+echo 'copied.'
+echo
+
+echo '# rsyncing built site to hosting directory'
+sudo rsync \
+    --verbose \
+    --checksum \
+    --recursive \
+    --links \
+    --perms \
+    --devices \
+    --specials \
+    --delete-during \
+    --delete-excluded \
+    --prune-empty-dirs \
+    --human-readable \
+    --human-readable \
+    --human-readable \
+    --itemize-changes \
+    _site/* /var/www/caselaw/
+echo
+
+popd >/dev/null
+popd >/dev/null
