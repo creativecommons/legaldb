6150d2c66e6f5a75d91e8c123543f542746f9de0
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2020-07-01 16:51:10 -0400

    Order lists of cases and scholarship

--- legal_db/admin.py
+++ legal_db/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-
 from .models import Case, FAQ, Link, Scholarship
 
 
 admin.site.register(Case)
-admin.site.register(FAQ)
 admin.site.register(Link)
 admin.site.register(Scholarship)
+
+
+class FAQAdmin(admin.ModelAdmin):
+    fields = ("question", "answer", "notes")
+    list_display = ("question", "order", "updated_at")
+
+
+admin.site.register(FAQ, FAQAdmin)
--- legal_db/views.py
+++ legal_db/views.py
@@ -12,13 +12,15 @@ def index(request):
 class CaseListView(ListView):
     template_name = "legal_db/case/index.html"
     context_object_name = "cases"
-    queryset = Case.objects.filter(status=Case.Status.PUBLISHED).only(
-        "country", "name", "license", "decision_year"
+    queryset = (
+        Case.objects.filter(status=Case.Status.PUBLISHED)
+        .only("country", "name", "license", "decision_year")
+        .order_by("country", "name")
     )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tags"] = Tag.objects.exclude(scholarship=None)
+        context["tags"] = Tag.objects.exclude(case=None)
         return context
 
 
@@ -37,8 +39,10 @@ class CaseDetailView(DetailView):
 class ScholarshipListView(ListView):
     template_name = "legal_db/scholarship/index.html"
     context_object_name = "scholarships"
-    queryset = Scholarship.objects.filter(status=Scholarship.Status.PUBLISHED).only(
-        "title", "authors", "license", "publication_year"
+    queryset = (
+        Scholarship.objects.filter(status=Scholarship.Status.PUBLISHED)
+        .only("title", "authors", "license", "publication_year")
+        .order_by("-publication_year", "title")
     )
 
     def get_context_data(self, **kwargs):
