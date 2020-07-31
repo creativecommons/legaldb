1e6130f3973fc244686f2ac5702b580bd776962f
Krystle Salazar <krystle.salazar@ciens.ucv.ve>
2020-07-31 16:48:30 -0400

    Convert answers of FAQ to Markdown

--- caselaw/urls.py
+++ caselaw/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("", include("legal_db.urls")),
     path("admin/", admin.site.urls),
+    url(r"^markdownx/", include("markdownx.urls")),
 ]
