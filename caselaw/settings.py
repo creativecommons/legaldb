59cedc9a90e7114313484d6d6bd31f88564bcb22
Darkshredder <srivastavayash58@gmail.com>
2021-02-20 04:47:04 +0530

    Initialized i18n and added spanish support to cases page

--- caselaw/settings.py
+++ caselaw/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
