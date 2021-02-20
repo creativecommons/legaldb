62a949789f9efe9b3d0ddbace46c0589cd09f390
Darkshredder <srivastavayash58@gmail.com>
2021-02-20 17:42:10 +0530

    Changed position of LocaleMiddleware,Moved it on top of common middleware

--- caselaw/settings.py
+++ caselaw/settings.py
@@ -52,8 +52,8 @@ MIDDLEWARE = [
     # SecurityMiddleware must be listed before other middleware
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
