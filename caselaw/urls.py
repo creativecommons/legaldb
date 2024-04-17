8abb2b3cf09709f4c746e4e86ad511d8621329a4
Timid Robot Zehta <timid@creativecommons.org>
2024-04-17 11:48:33 -0700

    replace url() with re_path()

--- caselaw/urls.py
+++ caselaw/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 # Third-party
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("", include("legal_db.urls")),
     path("admin/", admin.site.urls),
-    url(r"^markdownx/", include("markdownx.urls")),
+    re_path(r"^markdownx/", include("markdownx.urls")),
 ]
