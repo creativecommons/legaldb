71545ff3e97bf66ae46f06ac0eb4792a2049fcc5
Joy A <joy.gold13@gmail.com>
2024-10-10 12:57:11 +0100

    Align footer with Vocabulary design system requirement

--- legal_db/static/vocabulary/js/vocabulary.js
+++ legal_db/static/vocabulary/js/vocabulary.js
@@ -2,25 +2,20 @@ const exploreButton = document.querySelector('button.explore');
 const explorePanel = document.querySelector('.explore-panel');
 
 // explorePanel.classList.add('hide');
-if(exploreButton!==null && explorePanel!==null) {
-    exploreButton.addEventListener('click', (event) => {
-        explorePanel.classList.toggle('expand');
-        // explorePanel.classList.toggle('hide');
-    });
-}
 
+exploreButton.addEventListener('click', (event) => {
+    explorePanel.classList.toggle('expand');
+    // explorePanel.classList.toggle('hide');
+});
 
 
 const menuButton = document.querySelector('button.expand-menu');
 const menuPanel = document.querySelector('.primary-menu');
 
-if(menuButton !== null && menuPanel !== null) {
-    menuButton.addEventListener('click', (event) => {
-        menuPanel.classList.toggle('expand');
-        // explorePanel.classList.toggle('hide');
-    });
-}
-
+menuButton.addEventListener('click', (event) => {
+    menuPanel.classList.toggle('expand');
+    // explorePanel.classList.toggle('hide');
+});
 
 
 const attributionButton = document.querySelector('button.expand-attribution');
